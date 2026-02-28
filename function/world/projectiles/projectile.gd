221f0c10de0ec974b29132fca2c8fa481418ad55
aapolipponen <aapo.lipponen@gmail.com>
2026-02-28 20:12:48 +0200

    hotfix

--- function/world/projectiles/projectile.gd
+++ function/world/projectiles/projectile.gd
@@ -17,7 +17,6 @@ func initialize(start_pos: Vector2, direction: Vector2, p_circle_tool: TileTool)
     velocity = direction.normalized() * speed
     circle_tool = p_circle_tool
     body_entered.connect(_on_body_entered)
-    connect("body_entered", body_enter)
     rotation = direction.angle()
 
 func _physics_process(delta: float):
