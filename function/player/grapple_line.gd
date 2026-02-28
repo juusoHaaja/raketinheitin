f412a9c006e5f6e3b344c136be9371e36c08904c
Jonde1 <jonde.tiihonen@gmail.com>
2026-02-28 16:28:56 +0200

    debug player controller & grapple thing start

---
+++ function/player/grapple_line.gd
@@ -0,0 +1,45 @@
+extends Node2D
+class_name GrappleLine
+
+var anchor: Vector2 = Vector2.ZERO
+
+
+@onready var line2d: Line2D = $Line2D
+@onready var raycast: RayCast2D = $RayCast2D
+@onready var player: Player = get_parent().get_parent()
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    position = player.global_position
+
+func _physics_process(delta: float) -> void:
+    update()
+
+func update():
+    position = player.global_position
+    raycast.target_position = to_local(anchor)
+
+    raycast.force_raycast_update()
+
+    if raycast.is_colliding():
+        if raycast.get_collision_point().distance_squared_to(global_position) / get_length_squared() < 0.8 * 0.8:
+            queue_free()
+
+    line2d.set_point_position(1, to_local(anchor))
+
+func update_anchor(global_pos: Vector2):
+    anchor = global_pos
+
+"""func _unhandled_input(event: InputEvent):
+    if event is InputEventMouseButton and event.pressed:
+        if event.button_index == MOUSE_BUTTON_MIDDLE:
+            update_anchor(get_global_mouse_position())"""
+
+func get_angle_vector():
+    return (anchor - global_position).normalized()
+
+func get_length():
+    return (anchor - global_position).length()
+
+func get_length_squared():
+    return (anchor - global_position).length_squared()
--- function/player/player.gd
+++ function/player/player.gd
@@ -1,4 +1,9 @@
 extends RigidBody2D
+class_name Player
+
+
+@onready var ground_raycast: RayCast2D = $GroundRaycast
+@onready var line_holder = $Lines
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,4 +16,23 @@ func _process(delta: float) -> void:
     pass
 
 func _physics_process(delta: float) -> void:
-    pass
+    var wish_dir = Input.get_vector("left", "right", "up", "down")
+
+    if grounded():
+        if abs(linear_velocity.x) < 500.0:
+            apply_central_force(Vector2(wish_dir.x, 0) * 100000 * delta)
+
+        if wish_dir.y < 0.0:
+            apply_central_force(Vector2.UP * 10000)
+        
+            
+
+    var lines = line_holder.get_children()
+
+    if wish_dir.length() > 0.1:
+        for line: GrappleLine in lines:
+            line.get_angle_vector()
+        
+
+func grounded():
+    return ground_raycast.is_colliding()
